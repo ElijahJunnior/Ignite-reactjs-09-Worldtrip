004eaa6a265ce498cc49e4a5f7eff6b30ee004c7
Elias Junior <elijah.junnior@outlook.com>
2022-02-03 07:08:46 -0300

    intaling chakra-ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,9 @@ export default class MyDocument extends Document {
                     <title>Worldtrip</title>
                     <meta name="description" content="" />
                     <link rel="icon" href="/favicon.ico" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
