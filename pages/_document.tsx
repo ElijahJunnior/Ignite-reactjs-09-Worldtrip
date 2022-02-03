81c93ef0368d3d70a272348f9f4b6456296d7e1f
Elias Junior <elijah.junnior@outlook.com>
2022-02-03 06:37:15 -0300

    cleaning the project template

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>Worldtrip</title>
+                    <meta name="description" content="" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
