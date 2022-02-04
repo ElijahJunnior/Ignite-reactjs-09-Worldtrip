03653ba2f9a21ff13502e2c6e5cbe6ae9e17f738
Elias Junior <elijah.junnior@outlook.com>
2022-02-04 07:05:05 -0300

    testing extend theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
+import { Header } from '../components/Header'
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Header />
       <h1>Hello World</h1>
-    </div>
+    </>
   )
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,18 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, theme as baseTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+    colors: {
+        dark: {
+            body: baseTheme.colors.black,
+            text: baseTheme.colors.red[500],
+            info: baseTheme.colors.green[500]
+        },
+        light: {
+            body: baseTheme.colors.white,
+            text: '',
+            info: ''
+        }
+    },
     fonts: {
         heading: 'Poppins',
         body: 'Poppins'
