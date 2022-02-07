8169da08de203727d600684024fa36c394f494f4
Elias Junior <elijah.junnior@outlook.com>
2022-02-07 15:03:40 -0300

    testing banner image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Text, Heading } from '@chakra-ui/react';
 import Image from 'next/image';
 
 import { Header } from '../components/Header'
@@ -8,11 +8,14 @@ export default function Home() {
   return (
     <>
       <Header />
-
-      <Flex position="relative" >
-        <Box w="100vw" h="21rem" position="absolute" top="0" left="0">
-          <Image src={BannerImg} layout="fill" />
+      <Flex position="relative">
+        {/* <Box zIndex="1" bg='color'>
+          <Heading>Elias 123</Heading>
+          <Text>Elias 123</Text>
         </Box>
+        <Box w="100vw" height="21rem" position="absolute" top="0" left="0" overflow="hidden" zIndex="1">
+          <Image src={BannerImg} layout="responsive" alt="banner" />
+        </Box> */}
       </Flex>
     </>
   )
