8fc8343606b0e666ec5c6b34a8dc9c6f24b487b0
Elias Junior <elijah.junnior@outlook.com>
2022-02-08 06:57:13 -0300

    creating banner to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,34 @@
-import { Box, Flex, Text, Heading } from '@chakra-ui/react';
+import { Box, Flex, Text, Heading, LightMode, HStack } from '@chakra-ui/react';
 import Image from 'next/image';
 
 import { Header } from '../components/Header'
-import BannerImg from '../../public/Images/homeBanner.png'
+import AirplaneImg from '../../public/Images/airplane.svg'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Flex position="relative">
-        {/* <Box zIndex="1" bg='color'>
-          <Heading>Elias 123</Heading>
-          <Text>Elias 123</Text>
-        </Box>
-        <Box w="100vw" height="21rem" position="absolute" top="0" left="0" overflow="hidden" zIndex="1">
-          <Image src={BannerImg} layout="responsive" alt="banner" />
-        </Box> */}
+      <Flex
+        w="100vw" h="21rem" justify="center"
+        backgroundImage="images/homeBanner.png"
+      >
+        <Flex w="100%" maxW={1280} position="relative">
+          <Box maxW="32rem" pt="5rem" px="1.5rem" color="light.text">
+            <Heading mb="1.25rem" fontWeight="500" fontSize="36px" lineHeight="54px">
+              5 Continentes,<br />
+              infinitas possibilidades.
+            </Heading>
+            <Text fontWeight="400" fontSize="20px" fontHeight="30px">
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+            </Text>
+          </Box>
+          <Box
+            width="26.rem" transform="rotate(3deg)"
+            position="absolute" right="1.5rem" bottom="-2.5rem"
+          >
+            <Image src={AirplaneImg} />
+          </Box>
+        </Flex>
       </Flex>
     </>
   )
