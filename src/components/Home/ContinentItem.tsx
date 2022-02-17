c83160a373a5990492cc29964139bf11302fbf65
Elias Junior <elijah.junnior@outlook.com>
2022-02-17 13:45:55 -0300

    treating the images of continents

--- src/components/Home/ContinentItem.tsx
+++ src/components/Home/ContinentItem.tsx
@@ -12,15 +12,15 @@ export function ContinentItem(props: IContinentItemProps) {
     return (
         <Flex
             h="100%" w="100%" flexDir="column" align="center" justify="center"
-            bgImage={props.imageSrc} bgSize="cover"
+            bgImage={props.imageSrc} bgSize="cover" bgPosition="center"
         >
             <Heading
-                color="light.text" pb="1rem"
+                color="light.text" pb="1rem" 
                 fontWeight="700" fontSize="48px" lineHeight="72px"
             >
                 {props.name}
             </Heading>
-            <Text color="light.info" fontWeight="700" fontSize="24px" lineHeight="36px">
+            <Text color="light.info" fontWeight="700" fontSize="24px" lineHeight="36px" >
                 {props.description}
             </Text>
         </Flex>
