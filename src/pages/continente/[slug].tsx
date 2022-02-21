0f5715eec5f33bd0bde5a18c6cc7d8d09754539e
Elias Junior <elijah.junnior@outlook.com>
2022-02-21 13:18:42 -0300

    creating continent page

--- src/pages/continente/[slug].tsx
+++ src/pages/continente/[slug].tsx
@@ -1,3 +1,4 @@
+import { Flex, Heading, Text, HStack, Box } from '@chakra-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 import { Header } from '../../components/Header'
@@ -10,8 +11,34 @@ export default function Continente(props) {
     return (
         <>
             <Header />
-            <h1>Hello World</h1>
-            <h2>{props.slug}</h2>
+            <Flex w="100%" h="500px" bg="dark.text">
+                <Flex w="100%" h="100%" maxW="1280px" m="0 auto" flexDir="column-reverse">
+                    <Heading fontSize="48px" fontWeight="600" lineHeight="72px" mb="3.625rem">
+                        {props.slug}
+                    </Heading>
+                </Flex>
+            </Flex>
+            <Flex w="100%" maxW="1280px" m="0 auto" flexDir="column">
+                <HStack spacing="42px">
+                <Text>
+                    A Europa é, por convenção, um dos seis continentes 
+                    do mundo. Compreendendo a península ocidental da 
+                    Eurásia, a Europa geralmente divide-se da Ásia a 
+                    leste pela divisória de águas dos montes Urais, o 
+                    rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
+                </Text>
+                <Box w="100px" h="100px" bg="dark.info">
+                    
+                </Box>
+                <Box w="120px"  h="100px" bg="dark.info">
+                    
+                </Box>
+                <Box w="186px"  h="100px" bg="dark.info">
+                    
+                </Box>
+
+                </HStack>
+            </Flex>
         </>
     )
 }
