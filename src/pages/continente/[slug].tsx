179d699316342557ed5f6444373bea91e896613f
Elias Junior <elijah.junnior@outlook.com>
2022-02-21 07:06:50 -0300

    creating getStaticProps functions on continents page

--- src/pages/continente/[slug].tsx
+++ src/pages/continente/[slug].tsx
@@ -1,10 +1,37 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { ParsedUrlQuery } from 'querystring'
 import { Header } from '../../components/Header'
 
-export default function Continente() {
+interface Iparams extends ParsedUrlQuery {
+    slug: string
+}
+
+export default function Continente(props) {
     return (
         <>
-        <Header />
+            <Header />
             <h1>Hello World</h1>
+            <h2>{props.slug}</h2>
         </>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: "blocking"
+    }
+}
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+    const { slug } = ctx.params as Iparams
+
+    return {
+        props: {
+            slug
+        },
+        revalidate: 60
+    }
 }
